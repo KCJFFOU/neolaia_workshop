14b359d4d738c80d258f15c365a179f170bfb1e7
KCJFFOU <102779872+KCJFFOU@users.noreply.github.com>
2025-06-12 11:36:33 +0200

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Indexy textů", layout="centered")
+st.set_page_config(page_title="Jazykové indexy textů", layout="centered")
 
 st.title("📋 Jazykové indexy jednotlivých textů")
 
-# Načtení dat
-df = pd.read_excel("capek-pca.xlsx")
+# Načtení Excelového souboru
+@st.cache_data
+def load_data():
+    return pd.read_excel("capek-pca.xlsx")
 
-# Výběr textu
+df = load_data()
+
+# Roller pro výběr textu
 text_names = df.iloc[:, 0].tolist()
 selected_text = st.selectbox("Vyber text:", text_names)
 
-# Výpis indexů pro vybraný text
+# Zobrazení indexů pro vybraný text
 selected_row = df[df.iloc[:, 0] == selected_text].iloc[:, 1:]
 st.write("### Hodnoty indexů:")
 st.dataframe(selected_row.T.rename(columns={selected_row.index[0]: "Hodnota"}))
